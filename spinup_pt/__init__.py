8970c20e58fbf22ea0902a81889dd60833746729
KongCDY <939779599@qq.com>
2019-10-21 15:57:01 +0800

    [update] add setup

--- spinup_pt/__init__.py
+++ spinup_pt/__init__.py
@@ -1,10 +1,13 @@
 # Algorithms
-# from spinup_pt.algos.vpg.vpg import vpg
-# from spinup_pt.algos.trpo.trpo import trpo
-# from spinup_pt.algos.ppo.ppo import ppo
+from spinup_pt.algos.vpg.vpg import vpg
+from spinup_pt.algos.trpo.trpo import trpo
+from spinup_pt.algos.ppo.ppo import ppo
+from spinup_pt.algos.ddpg.ddpg import ddpg
+from spinup_pt.algos.td3.td3 import td3
+from spinup_pt.algos.sac.sac import sac
 
 # Loggers
 from spinup_pt.utils.logx import Logger, EpochLogger
 
 # Version
-# from spinup.version import __version__
+from spinup_pt.version import __version__
